b1492d3745778185ed886b7e6b531cbdc757867e
Lucas Jellema <lucasjellema@gmail.com>
2025-01-28 22:08:27 +0100

    build result from filtering elements in a single collection

--- filter.js
+++ filter.js
@@ -2,7 +2,7 @@ const filterButton = document.getElementById('filterButton');
 const applyFilterButton = document.getElementById('applyFilter');
 const resetFilterButton = document.getElementById('resetFilter');
 const filterPanel = document.getElementById('filterPanel');
-const closePanelButton = document.getElementById('closePanelButton'); 
+const closePanelButton = document.getElementById('closePanelButton');
 const filterValueInput = document.getElementById('filterValue'); // .value.toLowerCase();
 const includeConnectedCheckBox = document.getElementById('includeConnected'); //.checked
 const includeSelectedCheckBox = document.getElementById('includeSelected'); //.checked
@@ -11,7 +11,7 @@ const includeSelectedCheckBox = document.getElementById('includeSelected'); //.c
 export const initializeFilter = (cy) => {
     filterButton.addEventListener('click', () => {
         filterPanel.style.display = 'block';
-    });    
+    });
 
     applyFilterButton.addEventListener('click', () => {
         applyFilter(cy);
@@ -22,45 +22,48 @@ export const initializeFilter = (cy) => {
     })
     closePanelButton.addEventListener('click', () => {
         closeFilterPanel();
-    });    
-} 
+    });
+}
 
 
-const closeFilterPanel= () =>  {
+const closeFilterPanel = () => {
     filterPanel.style.display = 'none';
-  }
+}
 
-  const resetFilter = (cy) =>  {
+const resetFilter = (cy) => {
     cy.elements().show()
     filterValueInput.value = '';
     includeConnectedCheckBox.checked = false;
-  }
+}
 
-  const applyFilter = (cy) =>  {
+const applyFilter = (cy) => {
     const filterValue = filterValueInput.value.trim();
     const includeConnected = includeConnectedCheckBox.checked;
     if (filterValue) {
         // Reset all elements to hidden first
         cy.elements().hide();
+
+        const theElements = cy.collection();
         // add currently selected elements
         if (includeSelectedCheckBox.checked) {
-            const selectedElements = cy.$(':selected');
-            selectedElements.show();
+
+            theElements.merge(cy.$(':selected'));
         }
         // https://js.cytoscape.org/#collection/traversing 
         // Select nodes with the matching prefix
         const matchedNodes = cy.nodes().filter((node) =>
             node.data('label').startsWith(filterValue)
         );
-
+        theElements.merge(matchedNodes);
         // Show matched nodes
-        matchedNodes.show();
+        theElements.show();
 
         if (includeConnected) {
             // Show nodes referenced from the matched nodes (through the edges connected to the matched nodes)
-            matchedNodes.connectedEdges().targets().show();
+            theElements.connectedEdges().targets().show();
         }
         // Show edges connected to the matched nodes
-        matchedNodes.connectedEdges().show();
+        theElements.connectedEdges().show();
 
-    }}
+    }
+}
